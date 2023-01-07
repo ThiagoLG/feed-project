c885799ddef0d963bc75686115eb97b4a16ea50b
Thiago Lourençon Ghebra <thiago,lg53@gmail.com>
2023-01-07 18:25:37 -0300

    creating Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
+import { Header } from "./components/Header"
+import styles from "./App.module.css"
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <Header />
+      <div className={styles.wrapper}>
+        <aside></aside>
+        <main>
+          <h1>Hello World</h1>
+          <h1>Hello World</h1>
+          <h1>Hello World</h1>
+          <h1>Hello World</h1>
+        </main>
+      </div>
+    </>
   )
 }
 
